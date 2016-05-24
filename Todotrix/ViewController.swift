a41245f31a2ea4bb7e484f93763a6cfa50e5ce47
ltebean <yucong1118@gmail.com>
2016-05-24 11:39:31 +0800

    Initial Commit

---
+++ Todotrix/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todotrix
+//
+//  Created by leo on 16/5/24.
+//  Copyright © 2016年 io.ltebean. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
